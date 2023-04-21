c21fe15218b0314a8ff6fa0fd7f3e686caa60d01
Khatantuul Batbold <batbold.k@northeastern.edu>
2023-04-21 11:12:00 -0400

    Major changes to RequestDirectory attributes and methods

--- Serve-app/src/Requests/RequestDirectory.java
+++ Serve-app/src/Requests/RequestDirectory.java
@@ -5,54 +5,117 @@
 package Requests;
 
 import Applicant.Application;
+import Organization.Organization;
 import Organization.ProductManagement.ProductOrder;
 import Organization.ServiceManagement.ServiceOrder;
+import Organization.ServicesOrganization;
 import UserAccount.UserAccount;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 /**
  *
  * @author khatna
  */
 public class RequestDirectory {
-    HashMap<String, Request> allRequests;
+    private HashMap<String, List<Request>> allRequests;
+    private HashMap<String, ApplicationRequest> conVolApplicationRequests;
+    private HashMap<String, ApplicationRequest> volApplicationRequests;
+    private HashMap<String, ApplicationRequest> clientApplicationRequests;
     
     public RequestDirectory(){
-        this.allRequests = new HashMap();
+        this.allRequests = new HashMap<>();
+        this.conVolApplicationRequests = new HashMap<>();
+        this.volApplicationRequests = new HashMap<>();
+        this.clientApplicationRequests = new HashMap<>();
     }
     
-     public ApplicationRequest createVolunteerApplicationRequest(UserAccount ua, Application app) {
-        ApplicationRequest appReq = new ApplicationRequest(ua,app);
-        allRequests.put("Volunteer Application",appReq);
+   public ApplicationRequest createConVolunteerApplicationRequest(Application app,Organization so) {
+        ApplicationRequest appReq = new ApplicationRequest(app,so);
+        List<Request> requests = allRequests.getOrDefault("Convenience Volunteers Applications", new ArrayList<>());
+        requests.add(appReq);
+        allRequests.put("Convenience Volunteers Applications", requests);
+        conVolApplicationRequests.put(appReq.getRequestID(), appReq);
         return appReq;
     }
+    public ApplicationRequest createVolunteerApplicationRequest(UserAccount ua, Application app, Organization so) {
+            ApplicationRequest appReq = new ApplicationRequest(ua,app,so);
+            List<Request> requests = allRequests.getOrDefault("Service Volunteers Applications", new ArrayList<>());
+            requests.add(appReq);
+            allRequests.put("Service Volunteers Applications", requests);
+            volApplicationRequests.put(appReq.getRequestID(), appReq);
+            return appReq;
+    }
+     
     
-    public ApplicationRequest createClientApplicationRequest(UserAccount ua, Application app) {
-        ApplicationRequest appReq = new ApplicationRequest(ua,app);
-        allRequests.put("Client Application",appReq);
+    public ApplicationRequest createClientApplicationRequest(Application app) {
+        ApplicationRequest appReq = new ApplicationRequest(app);
+        List<Request> requests = allRequests.getOrDefault("Client Applications", new ArrayList<>());
+        requests.add(appReq);
+        allRequests.put("Client Applications", requests);
+        clientApplicationRequests.put(appReq.getRequestID(), appReq);
         return appReq;
     }
      
+    public ConvenienceRequest createConvenienceRequest(UserAccount ua, ProductOrder po) {
+    ConvenienceRequest conReq = new ConvenienceRequest(ua,po);
+    List<Request> requests = allRequests.getOrDefault("Convenience", new ArrayList<>());
+    requests.add(conReq);
+    allRequests.put("Convenience", requests);
+    return conReq;
+}
      
-     public ConvenienceRequest createConvenienceRequest(UserAccount ua, ProductOrder po) {
-         ConvenienceRequest conReq = new ConvenienceRequest(ua,po);
-        allRequests.put("Convenience",conReq);
-        return conReq;
+//     public ConvenienceRequest createConvenienceRequest(UserAccount ua, ProductOrder po) {
+//         ConvenienceRequest conReq = new ConvenienceRequest(ua,po);
+//        allRequests.put("Convenience",conReq);
+//        return conReq;
+//    }
+//     public HealthRequest createHealthRequest(UserAccount ua, ServiceOrder so) {
+//         HealthRequest healthReq = new HealthRequest(ua,so);
+//        allRequests.put("Health",healthReq);
+//        return healthReq;
+//    }
+//     public ConnectionRequest createConnectionRequest(UserAccount ua, ServiceOrder so) {
+//         ConnectionRequest connectionReq = new ConnectionRequest(ua,so);
+//        allRequests.put("Connection",connectionReq);
+//        return connectionReq;
+//    }
+//     public LegalRequest createLegalRequest(UserAccount ua, ServiceOrder so) {
+//         LegalRequest legalReq = new LegalRequest(ua,so);
+//        allRequests.put("Legal",legalReq);
+//        return legalReq;
+//    }
+
+    public HashMap<String, List<Request>> getAllRequests() {
+        return allRequests;
     }
-     public HealthRequest createHealthRequest(UserAccount ua, ServiceOrder so) {
-         HealthRequest healthReq = new HealthRequest(ua,so);
-        allRequests.put("Health",healthReq);
-        return healthReq;
+
+    public HashMap<String, ApplicationRequest> getConVolApplicationRequests() {
+        return conVolApplicationRequests;
     }
-     public ConnectionRequest createConnectionRequest(UserAccount ua, ServiceOrder so) {
-         ConnectionRequest connectionReq = new ConnectionRequest(ua,so);
-        allRequests.put("Connection",connectionReq);
-        return connectionReq;
+
+    public HashMap<String, ApplicationRequest> getVolApplicationRequests() {
+        return volApplicationRequests;
     }
-     public LegalRequest createLegalRequest(UserAccount ua, ServiceOrder so) {
-         LegalRequest legalReq = new LegalRequest(ua,so);
-        allRequests.put("Legal",legalReq);
-        return legalReq;
+
+    public HashMap<String, ApplicationRequest> getClientApplicationRequests() {
+        return clientApplicationRequests;
     }
+
+    
+     
+    public Request findRequestById(String id) {
+    for (List<Request> requests : allRequests.values()) {
+        for (Request request : requests) {
+            if (request.getRequestID().equals(id)) {
+                return request;
+            }
+        }
+    }
+    return null;
+    }
+
+     
     
 }
