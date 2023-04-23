f1aff51768533d26862e1ea8166db293532ba5f9
Khatantuul Batbold <batbold.k@northeastern.edu>
2023-04-23 14:41:06 -0400

    Fixing merge conflict

--- Serve-app/src/UI/ApplicationManagerWorkArea/ApplicationManagerJFrame.java
+++ Serve-app/src/UI/ApplicationManagerWorkArea/ApplicationManagerJFrame.java
@@ -12,18 +12,19 @@ import UserAccount.UserAccount;
 
 /**
  *
- * @author siqiyang
+ * @author khatna
  */
 public class ApplicationManagerJFrame extends javax.swing.JFrame {
+
+    /**
+     * Creates new form ApplicationManagerJFrame
+     */
     private Network appSystem;
     private UserAccount useraccount;
     Enterprise enterprise;
     Organization organization;
-    /**
-     * Creates new form ApplicationManagerJFrame
-     */
     public ApplicationManagerJFrame(Network appSystem, Enterprise enterprise, Organization organization,UserAccount useraccount) {
-        initComponents();        
+         initComponents();        
         this.setVisible(true);
         this.setLocationRelativeTo(null);
         this.appSystem = appSystem;
@@ -32,14 +33,8 @@ public class ApplicationManagerJFrame extends javax.swing.JFrame {
         this.organization = organization;
         jSplitPane1.setDividerSize(1);
         
-        displayLoginInfo();
     }
-    
-    public void displayLoginInfo(){
-        jLabel5.setText(String.valueOf(this.useraccount.getPerson().getName()));
-        jLabel2.setText(String.valueOf(this.useraccount.getRole()));
 
-    }
 
     /**
      * This method is called from within the constructor to initialize the form.
@@ -51,122 +46,110 @@ public class ApplicationManagerJFrame extends javax.swing.JFrame {
     private void initComponents() {
 
         jSplitPane1 = new javax.swing.JSplitPane();
-        controlPanel = new javax.swing.JPanel();
-        backBtn = new javax.swing.JButton();
-        volApplicationsBtn = new javax.swing.JButton();
-        clientApplicationsBtn = new javax.swing.JButton();
-
-        jButton1 = new javax.swing.JButton();
-        jLabel6 = new javax.swing.JLabel();
-        jLabel5 = new javax.swing.JLabel();
-        jLabel2 = new javax.swing.JLabel();
-        jLabel8 = new javax.swing.JLabel();
-
         jPanel1 = new javax.swing.JPanel();
+        jButton1 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
+        jButton3 = new javax.swing.JButton();
+        jPanel2 = new javax.swing.JPanel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        setBounds(new java.awt.Rectangle(0, 25, 1300, 800));
 
-        jSplitPane1.setPreferredSize(new java.awt.Dimension(1300, 800));
+        jPanel1.setBackground(new java.awt.Color(255, 255, 255));
 
-        controlPanel.setBackground(new java.awt.Color(255, 255, 255));
-        controlPanel.setPreferredSize(new java.awt.Dimension(150, 800));
-        controlPanel.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
-
-        backBtn.setBackground(new java.awt.Color(236, 100, 44));
-        backBtn.setFont(new java.awt.Font("Helvetica Neue", 1, 14)); // NOI18N
-        backBtn.setForeground(new java.awt.Color(255, 255, 255));
-        backBtn.setText("LOGOUT");
-        backBtn.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                backBtnActionPerformed(evt);
-            }
-        });
-        controlPanel.add(backBtn, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 40, 160, 40));
-
-
-        volApplicationsBtn.setText("<html><body style='width: 80px; text-align: left;'>Volunteer Applications </body></html>\n");
-
-        volApplicationsBtn.setPreferredSize(new java.awt.Dimension(1300, 800));
-        volApplicationsBtn.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                volApplicationsBtnActionPerformed(evt);
-            }
-        });
-
-        controlPanel.add(volApplicationsBtn, new org.netbeans.lib.awtextra.AbsoluteConstraints(-10, 130, 170, 50));
-
-
-        clientApplicationsBtn.setFont(new java.awt.Font("Helvetica Neue", 1, 14)); // NOI18N
-        clientApplicationsBtn.setForeground(new java.awt.Color(236, 100, 44));
-        clientApplicationsBtn.setText("<html><body style='width: 50px; text-align: center;'>Client Applications </body></html> ");
-        clientApplicationsBtn.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                clientApplicationsBtnActionPerformed(evt);
-            }
-        });
-
-        controlPanel.add(clientApplicationsBtn, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 220, 160, -1));
-
-
+        jButton1.setBackground(new java.awt.Color(236, 100, 44));
         jButton1.setFont(new java.awt.Font("Helvetica Neue", 1, 14)); // NOI18N
-        jButton1.setForeground(new java.awt.Color(236, 100, 44));
-        jButton1.setText("<html><body style='width: 50px; text-align: center;'>Volunteer Applications </body></html> ");
+        jButton1.setForeground(new java.awt.Color(255, 255, 255));
+        jButton1.setText("Logout");
         jButton1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton1ActionPerformed(evt);
             }
         });
 
-        controlPanel.add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 300, 160, -1));
+        jButton2.setFont(new java.awt.Font("Helvetica Neue", 1, 14)); // NOI18N
+        jButton2.setForeground(new java.awt.Color(236, 100, 44));
+        jButton2.setText("Volunteer Applications");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
 
-        jLabel6.setFont(new java.awt.Font("Helvetica Neue", 1, 13)); // NOI18N
-        jLabel6.setForeground(new java.awt.Color(236, 100, 44));
-        jLabel6.setText("Hello ");
-        controlPanel.add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 380, -1, -1));
+        jButton3.setFont(new java.awt.Font("Helvetica Neue", 1, 14)); // NOI18N
+        jButton3.setForeground(new java.awt.Color(236, 100, 44));
+        jButton3.setText("Client Applications");
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3ActionPerformed(evt);
+            }
+        });
 
-        jLabel5.setForeground(new java.awt.Color(236, 100, 44));
-        jLabel5.setText("jLabel1");
-        controlPanel.add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 400, -1, -1));
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jButton1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jButton2)
+                            .addGroup(jPanel1Layout.createSequentialGroup()
+                                .addGap(10, 10, 10)
+                                .addComponent(jButton3)))
+                        .addGap(0, 0, Short.MAX_VALUE)))
+                .addContainerGap())
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(69, 69, 69)
+                .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 39, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(98, 98, 98)
+                .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(39, 39, 39)
+                .addComponent(jButton3, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(474, Short.MAX_VALUE))
+        );
 
-        jLabel2.setForeground(new java.awt.Color(236, 100, 44));
-        jLabel2.setText("jLabel1");
-        controlPanel.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 450, -1, -1));
+        jSplitPane1.setLeftComponent(jPanel1);
 
-        jLabel8.setFont(new java.awt.Font("Helvetica Neue", 1, 13)); // NOI18N
-        jLabel8.setForeground(new java.awt.Color(236, 100, 44));
-        jLabel8.setText("Role:");
-        controlPanel.add(jLabel8, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 430, -1, -1));
+        jPanel2.setBackground(new java.awt.Color(255, 255, 255));
 
+        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
+        jPanel2.setLayout(jPanel2Layout);
+        jPanel2Layout.setHorizontalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 1104, Short.MAX_VALUE)
+        );
+        jPanel2Layout.setVerticalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 779, Short.MAX_VALUE)
+        );
 
-        jSplitPane1.setTopComponent(controlPanel);
-
-        jPanel1.setBackground(new java.awt.Color(255, 255, 255));
-        jPanel1.setPreferredSize(new java.awt.Dimension(1150, 800));
-        jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
-        jSplitPane1.setRightComponent(jPanel1);
+        jSplitPane1.setRightComponent(jPanel2);
 
         getContentPane().add(jSplitPane1, java.awt.BorderLayout.CENTER);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void backBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_backBtnActionPerformed
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
-
         this.setVisible(false);
         new MainJFrame(appSystem, enterprise,organization,useraccount);
-    }//GEN-LAST:event_backBtnActionPerformed
+    }//GEN-LAST:event_jButton1ActionPerformed
 
-    private void volApplicationsBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_volApplicationsBtnActionPerformed
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
         // TODO add your handling code here:
         jSplitPane1.setRightComponent(new SpecialistManagementJPanel(appSystem, enterprise,organization,useraccount));
-    }//GEN-LAST:event_volApplicationsBtnActionPerformed
+    }//GEN-LAST:event_jButton2ActionPerformed
 
-    private void clientApplicationsBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_clientApplicationsBtnActionPerformed
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
         // TODO add your handling code here:
         jSplitPane1.setRightComponent(new ClientManagementJPanel(appSystem,enterprise,organization,useraccount));
-    }//GEN-LAST:event_clientApplicationsBtnActionPerformed
+    }//GEN-LAST:event_jButton3ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -198,24 +181,17 @@ public class ApplicationManagerJFrame extends javax.swing.JFrame {
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                //new ApplicationManagerJFrame().setVisible(true);
+//                new ApplicationManagerJFrame().setVisible(true);
             }
         });
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton backBtn;
-    private javax.swing.JButton clientApplicationsBtn;
-    private javax.swing.JPanel controlPanel;
-
     private javax.swing.JButton jButton1;
-    private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel jLabel5;
-    private javax.swing.JLabel jLabel6;
-    private javax.swing.JLabel jLabel8;
-
+    private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton3;
     private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
     private javax.swing.JSplitPane jSplitPane1;
-    private javax.swing.JButton volApplicationsBtn;
     // End of variables declaration//GEN-END:variables
 }
